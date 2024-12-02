1f6444685f163ae2034db9c067f91990167f7617
Endoxidev <113648540+MetaMysteries8@users.noreply.github.com>
2024-12-02 12:54:31 -0700

    fixed bug in cheat

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ namespace SpriteKind {
     export const Ending = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    errortrigger = 1
-    info.changeCountdownBy(1)
-    music.play(music.createSoundEffect(WaveShape.Noise, 3900, 3500, 255, 0, 10, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    if (allowedtocheat == 1) {
+        errortrigger = 1
+        info.changeCountdownBy(1)
+        music.play(music.createSoundEffect(WaveShape.Noise, 3900, 3500, 255, 0, 10, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+    }
 })
 info.onCountdownEnd(function () {
     game.setGameOverPlayable(false, music.melodyPlayable(music.sonar), false)
@@ -13,6 +15,8 @@ info.onCountdownEnd(function () {
     game.gameOver(false)
 })
 let errortrigger = 0
+let allowedtocheat = 0
+allowedtocheat = 0
 errortrigger = 0
 music.stopAllSounds()
 music.play(music.melodyPlayable(music.spooky), music.PlaybackMode.InBackground)
@@ -44,6 +48,7 @@ game.setGameOverEffect(false, effects.slash)
 game.setGameOverMessage(true, "Escape Successful")
 game.setGameOverMessage(false, "You Ran Out Of Time...")
 let mySprite5 = 1
+allowedtocheat = 1
 game.onUpdate(function () {
     if (mySprite5 == 1) {
         Render.setSpriteAttribute(mySprite4, RCSpriteAttribute.ZPosition, 0)
@@ -59,6 +64,7 @@ game.onUpdate(function () {
 game.onUpdate(function () {
     if (mySprite.overlapsWith(mySprite3)) {
         if (errortrigger == 1) {
+            allowedtocheat = 0
             sprites.destroy(mySprite3, effects.ashes, 5000)
             info.stopCountdown()
             music.play(music.createSoundEffect(WaveShape.Noise, 1246, 1246, 0, 219, 1000, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
